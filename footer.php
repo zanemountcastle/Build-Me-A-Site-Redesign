3608811247a05bf7f18eb9a20848aa2aafa86e4e
zanemountcastle <zmtcastle@yahoo.com>
2016-03-15 15:00:49 +0000

    Initial commit

---
+++ footer.php
@@ -0,0 +1,7 @@
+		<footer class="footer">
+			<p>Copyright © Build Me A Site 2016. All rights reserved.</p>
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+			<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/script.js"></script>
+		</footer>
+	</div>
+</html>
---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Build Me A Site</title>
+		<?php wp_head(); ?>
+
+		<?php if( is_page_template('functions.php')  ) :?>
+			
+			<!-- CSS -->
+			<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/style.css?<?php echo time(); ?>" media="screen" type="text/css" />
+			
+			<!-- Google Fonts: Roboto Light (300), Regular (400), Medium (500), Bold (700) -->
+			<link href='https://fonts.googleapis.com/css?family=Roboto:400,500,300,700' rel='stylesheet' type='text/css'>
+
+		<?php else:?>
+			<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+		<?php endif;?>
+
+	</head>
+
+	<body>
